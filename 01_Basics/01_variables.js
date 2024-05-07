6334202afbbf53dba78916e7a5766607eac76392
RohitMate555 <168441299+RohitMate555@users.noreply.github.com>
2024-05-07 05:53:48 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_Id = 22222
+let accountEmail = "rohit@gmail.com"
+var accountPassword = "55555"
+accountCity = "Mumbai"
+let accountState
+
+ //account_Id = 333 // not allowed
+
+accountEmail="rohit555@gmail.com"
+accountPassword ="12345"
+accountCity ="Bengaluru"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.log(account_Id);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3) // we wre using node js , not browser
+console.log("Rohit")
+
+//number =2 to power 53
+//bigint
+//string =""
+//boolean =true/False
+//null = standalone value
+//undefined = means value not defined;example:-let state="";
+//symbol = unique
+
+//object
+
+console.log(typeof "Funny"); 
+console.log(typeof undefined); 
+console.log(typeof null); 
